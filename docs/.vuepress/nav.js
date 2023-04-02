9542a6402eebed0cb3a91c95d800877cd09fca4a
kamenrider41 <229335832@qq.com>
2023-04-02 20:18:35 +0800

    refactor: 重构了导航栏和侧边栏的内容

--- docs/.vuepress/nav.js
+++ docs/.vuepress/nav.js
@@ -2,17 +2,39 @@
  * @Author: 41
  * @Date: 2022-06-05 16:38:13
  * @LastEditors: 41
- * @LastEditTime: 2023-04-01 23:29:21
+ * @LastEditTime: 2023-04-02 19:53:52
  * @Description: 
  */
-const before = [
+const beforeWork = [
   {
     text: '科研成果记录', link: '/partDocs/worklog/before/research/research'
   },
   {
     text: 'css权威指南读书笔记', link: '/partDocs/worklog/before/css'
   },
-
+]
+const threeFore = [
+  { text: 'HTML', link: '/partDocs/html/html' },
+  { text: 'CSS', link: '/partDocs/css/css' },
+  { text: 'Javascript', link: '/partDocs/javascript/javascript' },
+]
+const twoFrame = [
+  { text: 'Vue', link: '/partDocs/vue/vue' },
+  { text: 'React', link: '/partDocs/react/react' },
+]
+const practiceStudy = [
+  { text: 'Charles学习', link: '/partDocs/internship/study/charles' },
+  { text: 'Git学习', link: '/partDocs/internship/study/git/peizhi' },
+  { text: 'TypeScript学习', link: '/partDocs/internship/study/typescript/type' },
+  { text: 'React学习', link: '/partDocs/internship/study/react/react' },
+  { text: 'Flutter学习', link: '/partDocs/internship/study/flutter/flutter' },
+  { text: 'ProseMirror学习', link: '/partDocs/internship/study/prosemirror/prosemirror' },
+]
+const praticeWrite = [
+  { text: 'Bootcamp学习笔记', link: '/partDocs/internship/write/bootcamp' },
+  { text: 'vscode好用插件', link: '/partDocs/internship/write/vscode' },
+  { text: '实习提问', link: '/partDocs/internship/write/question' },
+  { text: 'Bug回忆录', link: '/partDocs/internship/write/bug/bug' },
 ]
 module.exports = [
   {
@@ -23,33 +45,22 @@ module.exports = [
   }, {
     text: '前端知识点',
     items: [
-      { text: 'HTML', link: '/partDocs/html/html' },
-      { text: 'CSS', link: '/partDocs/css/css' },
-      { text: 'Javascript', link: '/partDocs/javascript/javascript' },
-      { text: 'Vue', link: '/partDocs/vue/vue' },
-      { text: 'React', link: '/partDocs/react/react' },
+      { text: '前端三项之力', items: threeFore },
+      { text: '前端两大框架', items: twoFrame },
     ]
   }, {
     text: '实习笔记',
     items: [
       { text: '写在前面', link: '/partDocs/internship/internship' },
-      { text: '实习提问', link: '/partDocs/internship/question' },
-      { text: 'Bootcamp学习笔记', link: '/partDocs/internship/bootcamp' },
-      { text: 'vscode好用插件', link: '/partDocs/internship/vscode' },
-      { text: 'Charles学习', link: '/partDocs/internship/charles' },
-      { text: 'Git学习', link: '/partDocs/internship/git/peizhi' },
-      { text: 'TypeScript学习', link: '/partDocs/internship/typescript/type' },
-      { text: 'React学习', link: '/partDocs/internship/react/react' },
-      { text: 'Flutter学习', link: '/partDocs/internship/flutter/flutter' },
-      { text: 'ProseMirror学习', link: '/partDocs/internship/prosemirror/prosemirror' },
-      { text: 'Bug回忆录', link: '/partDocs/internship/bug/bug' },
+      { text: '乱七八糟的学习', items: practiceStudy },
+      { text: '乱七八糟的笔记', items: praticeWrite },
     ]
   },
   {
     text: '工作日志',
     items: [
       { text: '写在前面', link: '/partDocs/worklog/worklog' },
-      { text: '正式工作前', items: before },
+      { text: '正式工作前', items: beforeWork },
     ]
   },
   {
--- docs/.vuepress/sidebar.js
+++ docs/.vuepress/sidebar.js
@@ -1,4 +1,108 @@
 
+const practiceStudy =
+{
+  title: '乱七八糟的学习',
+  collapsable: true,
+  sidebarDepth: 1,
+  children: [
+    ['study/charles', 'Charles学习'],
+    {
+      title: 'Git学习',
+      collapsable: true,
+      sidebarDepth: 1,
+      children: [
+        ['study/git/peizhi', 'Git配置'],
+        ['study/git/ziliao', 'Git资料'],
+        {
+          title: 'Git主要',
+          collapsable: true,
+          sidebarDepth: 1,
+          children: [
+            ['study/git/main/jichu', '基础篇'],
+            ['study/git/main/gaoji', '高级篇'],
+            ['study/git/main/yidong', '移动提交记录'],
+            ['study/git/main/zaxiang', '杂项'],
+            ['study/git/main/huati', '高级话题'],
+          ]
+        },
+        {
+          title: 'Git远程',
+          collapsable: true,
+          sidebarDepth: 1,
+          children: [
+            ['study/git/long/1', 'PUSH&Pull'],
+            ['study/git/long/2', 'origin与它的周边'],
+          ]
+        },
+      ]
+    },
+    {
+      title: 'TypeScript学习',
+      collapsable: true,
+      sidebarDepth: 1,
+      children: [
+        ['study/typescript/type', '写在前面'],
+        ['study/typescript/rumen', '快速入门'],
+        ['study/typescript/jinjie', '进阶之泛型和装饰器'],
+      ]
+    },
+    {
+      title: 'React学习',
+      collapsable: true,
+      sidebarDepth: 1,
+      children: [
+        ['study/react/react', '写在前面'],
+        ['study/react/rumen', '快速入门'],
+        ['study/react/compose', '函数式编程学习笔记'],
+      ]
+    },
+    {
+      title: 'Flutter学习',
+      collapsable: true,
+      sidebarDepth: 1,
+      children: [
+        ['study/flutter/flutter', '写在前面'],
+        ['study/flutter/doctor', 'Flutter环境配置'],
+        ['study/flutter/hello', '小荷才露尖尖角'],
+        ['study/flutter/widget', 'widget是一切'],
+      ]
+    },
+    {
+      title: 'ProseMirror学习',
+      collapsable: true,
+      sidebarDepth: 1,
+      children: [
+        ['study/prosemirror/prosemirror', '写在前面'],
+        ['study/prosemirror/rumen', '编辑器从伤感到破局'],
+      ]
+    },
+  ]
+}
+
+const practiceWrite =
+{
+  title: '乱七八糟的笔记',
+  collapsable: true,
+  sidebarDepth: 1,
+  children: [
+    ['write/bootcamp', 'Bootcamp学习笔记'],
+    ['write/vscode', 'vscode好用插件'],
+    ['write/question', '实习提问'],
+    {
+      title: 'Bug回忆录',
+      collapsable: true,
+      sidebarDepth: 1,
+      children: [
+        ['write/bug/bug', '写在前面'],
+        ['write/bug/work', '研发'],
+        ['write/bug/git', 'Git'],
+        ['write/bug/flutter', 'Flutter'],
+        ['write/bug/other', '其他'],
+      ]
+    },
+  ]
+}
+
 module.exports = {
   '/partDocs/worklog/': [
     {
@@ -13,7 +117,6 @@ module.exports = {
           sidebarDepth: 1,
           children: [
             ['before/before', '写在前面'],
-            ['before/css', 'css权威指南读书笔记'],
             {
               title: '科研成果记录',
               collapsable: true,
@@ -23,10 +126,9 @@ module.exports = {
                 ['before/research/chua', '基于双信号源的蔡氏电路'],
                 ['before/research/3dqam', '新型三维星座图和OFDM-ROF系统设计'],
                 ['before/research/ml', '基于机器学习的三维星座解调'],
-                // ['git/main/yidong', '移动提交记录'],
               ]
             },
-            // ['before/research', '科研成果记录'],
+            ['before/css', 'css权威指南读书笔记'],
           ]
         },
       ]
@@ -39,92 +141,94 @@ module.exports = {
       sidebarDepth: 1,
       children: [
         ['internship', '写在前面'],
-        ['question', '实习提问'],
-        ['bootcamp', 'Bootcamp学习笔记'],
-        ['vscode', 'vscode好用插件'],
-        ['charles', 'Charles学习'],
-        {
-          title: 'Git学习',
-          collapsable: true,
-          sidebarDepth: 1,
-          children: [
-            ['git/peizhi', 'Git配置'],
-            ['git/ziliao', 'Git资料'],
-            {
-              title: 'Git主要',
-              collapsable: true,
-              sidebarDepth: 1,
-              children: [
-                ['git/main/jichu', '基础篇'],
-                ['git/main/gaoji', '高级篇'],
-                ['git/main/yidong', '移动提交记录'],
-                ['git/main/zaxiang', '杂项'],
-                ['git/main/huati', '高级话题'],
-              ]
-            },
-            {
-              title: 'Git远程',
-              collapsable: true,
-              sidebarDepth: 1,
-              children: [
-                ['git/long/1', 'PUSH&Pull'],
-                ['git/long/2', 'origin与它的周边'],
-              ]
-            },
-          ]
-        },
-        {
-          title: 'TypeScript学习',
-          collapsable: true,
-          sidebarDepth: 1,
-          children: [
-            ['typescript/type', '写在前面'],
-            ['typescript/rumen', '快速入门'],
-            ['typescript/jinjie', '进阶之泛型和装饰器'],
-          ]
-        },
-        {
-          title: 'React学习',
-          collapsable: true,
-          sidebarDepth: 1,
-          children: [
-            ['react/react', '写在前面'],
-            ['react/rumen', '快速入门'],
-            ['react/compose', '函数式编程学习笔记'],
-          ]
-        },
-        {
-          title: 'Flutter学习',
-          collapsable: true,
-          sidebarDepth: 1,
-          children: [
-            ['flutter/flutter', '写在前面'],
-            ['flutter/doctor', 'Flutter环境配置'],
-            ['flutter/hello', '小荷才露尖尖角'],
-            ['flutter/widget', 'widget是一切'],
-          ]
-        },
-        {
-          title: 'ProseMirror学习',
-          collapsable: true,
-          sidebarDepth: 1,
-          children: [
-            ['prosemirror/prosemirror', '写在前面'],
-            ['prosemirror/rumen', '编辑器从伤感到破局'],
-          ]
-        },
-        {
-          title: 'Bug回忆录',
-          collapsable: true,
-          sidebarDepth: 1,
-          children: [
-            ['bug/bug', '写在前面'],
-            ['bug/work', '研发'],
-            ['bug/git', 'Git'],
-            ['bug/flutter', 'Flutter'],
-            ['bug/other', '其他'],
-          ]
-        },
+        practiceStudy,
+        practiceWrite
+        // ['question', '实习提问'],
+        // ['bootcamp', 'Bootcamp学习笔记'],
+        // ['vscode', 'vscode好用插件'],
+        // ['charles', 'Charles学习'],
+        // {
+        //   title: 'Git学习',
+        //   collapsable: true,
+        //   sidebarDepth: 1,
+        //   children: [
+        //     ['git/peizhi', 'Git配置'],
+        //     ['git/ziliao', 'Git资料'],
+        //     {
+        //       title: 'Git主要',
+        //       collapsable: true,
+        //       sidebarDepth: 1,
+        //       children: [
+        //         ['git/main/jichu', '基础篇'],
+        //         ['git/main/gaoji', '高级篇'],
+        //         ['git/main/yidong', '移动提交记录'],
+        //         ['git/main/zaxiang', '杂项'],
+        //         ['git/main/huati', '高级话题'],
+        //       ]
+        //     },
+        //     {
+        //       title: 'Git远程',
+        //       collapsable: true,
+        //       sidebarDepth: 1,
+        //       children: [
+        //         ['git/long/1', 'PUSH&Pull'],
+        //         ['git/long/2', 'origin与它的周边'],
+        //       ]
+        //     },
+        //   ]
+        // },
+        // {
+        //   title: 'TypeScript学习',
+        //   collapsable: true,
+        //   sidebarDepth: 1,
+        //   children: [
+        //     ['typescript/type', '写在前面'],
+        //     ['typescript/rumen', '快速入门'],
+        //     ['typescript/jinjie', '进阶之泛型和装饰器'],
+        //   ]
+        // },
+        // {
+        //   title: 'React学习',
+        //   collapsable: true,
+        //   sidebarDepth: 1,
+        //   children: [
+        //     ['react/react', '写在前面'],
+        //     ['react/rumen', '快速入门'],
+        //     ['react/compose', '函数式编程学习笔记'],
+        //   ]
+        // },
+        // {
+        //   title: 'Flutter学习',
+        //   collapsable: true,
+        //   sidebarDepth: 1,
+        //   children: [
+        //     ['flutter/flutter', '写在前面'],
+        //     ['flutter/doctor', 'Flutter环境配置'],
+        //     ['flutter/hello', '小荷才露尖尖角'],
+        //     ['flutter/widget', 'widget是一切'],
+        //   ]
+        // },
+        // {
+        //   title: 'ProseMirror学习',
+        //   collapsable: true,
+        //   sidebarDepth: 1,
+        //   children: [
+        //     ['prosemirror/prosemirror', '写在前面'],
+        //     ['prosemirror/rumen', '编辑器从伤感到破局'],
+        //   ]
+        // },
+        // {
+        //   title: 'Bug回忆录',
+        //   collapsable: true,
+        //   sidebarDepth: 1,
+        //   children: [
+        //     ['bug/bug', '写在前面'],
+        //     ['bug/work', '研发'],
+        //     ['bug/git', 'Git'],
+        //     ['bug/flutter', 'Flutter'],
+        //     ['bug/other', '其他'],
+        //   ]
+        // },
       ]
     }
   ],
